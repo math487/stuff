07d9772c4df5d42cd81f9b8d2bd165ccb1cdfae5
Mathieu <math487@gmail.com>
2016-05-25 02:33:50 -0400

    Add project files.

---
+++ tutorials.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tutorials.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ tutorials/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tutorials.MvcApplication" Language="C#" %>
